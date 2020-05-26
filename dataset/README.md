6547cc4c3cfedd85b50906c7bbbfcdf4e2825be6
Aaron Jones <p842k739@wichita.edu>
2020-05-25 22:48:52 -0500

    Added BASIC file structure and imports for the model.py file. First need to collect a dataset before any real model creation can begin.

---
+++ dataset/README.md
@@ -0,0 +1,2 @@
+# Deep-Learning-Text-Generator
+This folder is where a text data set will be placed once enough data is gathered.
---
+++ model.py
@@ -0,0 +1,12 @@
+import sys
+
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import subprocess
+
+print('TensorFlow version: {}'.format(tf.__version__))
